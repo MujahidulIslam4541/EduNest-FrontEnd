3beb15c3a47812fcd8398102db038d0088c3c9ee
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-05-08 23:17:38 +0600

    added teacher/admin section

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -3,10 +3,15 @@ import logo from '../../assets/Edu-Nest-Main-Logo-1.png'
 import { Link } from 'react-router'
 import { GiProgression } from "react-icons/gi";
 import { SiCoursera } from "react-icons/si";
-import { FaUserCircle } from "react-icons/fa";
+import { FaUserCircle, FaChalkboardTeacher, FaUsers } from "react-icons/fa";
 import { GrLogout } from "react-icons/gr";
+import { MdManageAccounts, MdOutlineAddHome } from "react-icons/md";
+import { PiStudent } from "react-icons/pi";
+import { RiAdminLine } from "react-icons/ri";
 
 const Dashboard = () => {
+    const teacher = true;
+
     return (
         <div className='flex '>
             {/* sidebar section */}
@@ -26,35 +31,88 @@ const Dashboard = () => {
                     </Link>
                 </div>
 
-                {/* Menu Links */}
-                <div className="space-y-4 px-4">
-                    <Link
-                        to="/dashboard/progress"
-                        className="flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7EB] transition duration-200 text-base font-medium"
-                    >
-                        <GiProgression className="text-xl" />
-                        My Progress
-                    </Link>
+                {teacher == true ?
 
-                    <Link
-                        to="/dashboard/my-courses"
-                        className="flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7EB] transition duration-200 text-base font-medium"
-                    >
-                        <SiCoursera className="text-xl" />
-                        My Courses
-                    </Link>
+                    // instructor/teacher section
+                    <div className='space-y-4 px-4'>
+                        <Link className='flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7E8] transition duration-200 text-base font-medium' >
+                            <RiAdminLine className='text-xl' /> Admin Home
+                        </Link>
 
-                    <Link
-                        to="/dashboard/profile"
-                        className="flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7EB] transition duration-200 text-base font-medium"
-                    >
-                        <FaUserCircle className="text-xl" />
-                        Profile
-                    </Link>
-                </div>
+
+                        <Link className='flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7E8] transition duration-200 text-base font-medium' >
+                            <MdOutlineAddHome className='text-xl' /> Add New Course
+                        </Link>
+
+                        <Link className='flex items-center gap-3 px-6 py-2 rounded-xl bg-[#F3F4F6] hover:bg-[#E5E7E8] transition duration-200 text-base font-medium'>
+                            <FaChalkboardTeacher className='text-xl' /> My Courses
+                        </Link>
+
+                        <Link className='flex items-center gap-3 px-4 py-2 bg-[#F3F4F6] hover:bg-[#E5E7E8] transition duration-200 text-base font-medium'>
+                            <PiStudent className='text-xl' /> Enrolled Student
+                        </Link>
+
+                        <Link className='flex items-center gap-3 px-4 py-2 bg-[#F3F4F6] hover:bg-[#E5E7E8] transition duration-200 text-base font-medium'>
+                            <FaUsers className='text-xl' /> Manage Users
+                        </Link>
+
+                        <Link className='flex items-center gap-3 px-4 py-2 bg-[#F3F4F6] hover:bg-[#E5E7E8] transition duration-200 text-base font-medium'>
+                            <MdManageAccounts className='text-xl' /> Manage Courses
+                        </Link>
+
+                        <Link
+                            to="/dashboard/instructor-requests"
+                            className="flex items-center gap-3 px-4 py-2 bg-[#F3F4F6] hover:bg-[#E5E7E8] transition duration-200 text-base font-medium"
+                        >
+                            <FaUsers className="text-xl" />
+                            Instructor Requests
+                        </Link>
+
+
+
+                        <Link className='flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7E8] transition duration-200 text-base font-medium'>
+                            <FaUserCircle className='text-xl' /> Admin Profile
+                        </Link>
+
+                    </div>
+                    :
+                    // student/user menu part
+                    < div className="space-y-4 px-4">
+                        <Link
+                            to="/dashboard/progress"
+                            className="flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7EB] transition duration-200 text-base font-medium"
+                        >
+                            <GiProgression className="text-xl" />
+                            My Progress
+                        </Link>
+
+                        <Link
+                            to="/dashboard/my-courses"
+                            className="flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7EB] transition duration-200 text-base font-medium"
+                        >
+                            <SiCoursera className="text-xl" />
+                            My Courses
+                        </Link>
+
+                        <Link
+                            to="/dashboard/become-instructor"
+                            className="flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7EB] transition duration-200 text-base font-medium"
+                        >
+                            <FaChalkboardTeacher className="text-xl" />
+                            Become an Instructor
+                        </Link>
+
+                        <Link
+                            to="/dashboard/profile"
+                            className="flex items-center gap-3 px-4 py-2 rounded-lg bg-[#F3F4F6] hover:bg-[#E5E7EB] transition duration-200 text-base font-medium"
+                        >
+                            <FaUserCircle className="text-xl" />
+                            Profile
+                        </Link>
+                    </div>}
 
                 {/* Logout Button */}
-                <div className="mt-76 px-4">
+                <div className="pt-76 px-4">
                     <button
                         className="flex items-center gap-3 w-full px-4 py-2 rounded-lg bg-red-100 text-red-600 hover:bg-red-200 transition duration-200 font-medium"
                     >
@@ -69,7 +127,7 @@ const Dashboard = () => {
                 <h2>Main Section</h2>
             </div>
 
-        </div>
+        </div >
     )
 }
 
