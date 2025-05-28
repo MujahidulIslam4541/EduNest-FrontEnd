e8b27da628705879c37e47c8bd31117d436b7933
MujahidulIslam4541 <156920899+MujahidulIslam4541@users.noreply.github.com>
2025-05-28 14:57:26 +0600

    user successfully deleted

--- src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
+++ src/Pages/Dashboard/ManageUsers/ManageUsers.jsx
@@ -1,36 +1,49 @@
 import { useEffect, useState } from "react";
+import UseAxiosSecure from "../../../components/hooks/UseAxiosSecuire/UseAxiosSecure";
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const ManageUsers = () => {
   const [users, setUsers] = useState([]);
+  const axiosSecure = UseAxiosSecure();
 
+  // Get all user form database
   useEffect(() => {
-    setUsers([
-      {
-        _id: "1",
-        name: "Rifat Islam",
-        email: "rifat@example.com",
-        role: "student",
-        degree: "BSc in CSE",
-        instructorRequest: true,
-      },
-      {
-        _id: "2",
-        name: "Nahid Hasan",
-        email: "nahid@example.com",
-        role: "student",
-        instructorRequest: false,
-      },
-      {
-        _id: "3",
-        name: "Arif Khan",
-        email: "arif@example.com",
-        role: "instructor",
-        degree: "MSc in CS",
-      },
-    ]);
+    axiosSecure('/users').then(res => {
+      setUsers(res.data)
+    }).catch(error => {
+      console.error('Failed to fetch users:', error);
+    })
+
   }, []);
 
- 
+  // delete user form database
+  const handleDeleteUser = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        axiosSecure.delete(`/users/${user?._id}`).then(res => {
+          if (res.data.deletedCount > 0) {
+            Swal.fire({
+              title: "Deleted!",
+              text: `${user?.name || "The user"} has been removed successfully.`,
+              icon: "success"
+            });
+          }
+        })
+      }
+    });
+  }
+
+
 
   return (
     <div className="p-6">
@@ -56,10 +69,11 @@ const ManageUsers = () => {
           <tbody>
             {users.map((user, index) => {
               const isInstructor = user.role === "instructor";
-              const hasRequested = user.instructorRequest;
+              const hasRequested = user.isInstructorRequest;
 
               return (
-                <tr key={user._id} className="border-t">
+                <tr key={user?._id} className="border-t">
+
                   <td className="px-6 py-4">{index + 1}</td>
                   <td className="px-6 py-4">{user.name}</td>
                   <td className="px-6 py-4">{user.email}</td>
@@ -70,24 +84,38 @@ const ManageUsers = () => {
                     ) : (
                       <span className="text-red-500 font-medium">Unavailable</span>
                     )}
+
                   </td>
 
 
                   <td className="px-6 py-4 text-center">
-                    {isInstructor ? (
-                      <span className="text-green-600 font-medium text-sm">
-                        Already Instructor
-                      </span>
-                    ) : hasRequested ? (
+                    <div className="flex justify-center items-center gap-2 flex-wrap">
+                      {isInstructor ? (
+                        <span className="text-green-600 font-medium text-sm">
+                          Already Instructor
+                        </span>
+                      ) : hasRequested ? (
+                        <button
+                          className="px-4 py-2 bg-[#84CC16] hover:bg-[#84CC18] text-white text-sm font-semibold rounded-lg shadow-md transition duration-300"
+                        >
+                          Make Instructor
+                        </button>
+                      ) : (
+                        <span className="text-gray-400 text-sm">—</span>
+                      )}
+
+                      {/* user delete button */}
                       <button
-                        className="px-4 py-2 bg-[#84CC16] hover:bg-[#84CC18] text-white text-sm font-semibold rounded-lg shadow-md  transition duration-300"
+                        onClick={() => handleDeleteUser(user)}
+                        className="flex items-center gap-2 px-3 py-2 bg-red-100 text-red-600 border border-red-300 hover:bg-red-200 hover:text-red-700 text-sm font-medium rounded-md transition duration-200"
                       >
-                        Make Instructor
+                        <FaTrashAlt className="text-base" />
+                        Delete
                       </button>
-                    ) : (
-                      <span className="text-gray-400 text-sm">—</span>
-                    )}
+                    </div>
                   </td>
+
+
                 </tr>
               );
             })}
